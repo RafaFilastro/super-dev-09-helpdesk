fef58306407cba815c18883a69859a530336911d
Rafael Filastro <rafael.filastro@gmail.com>
2026-09-21 22:16:33 -0300

    feat: Integrado frontend com a API tickets

---
+++ front-end/src/app/services/ticket.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { TicketResposta } from '../models/tickets.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketService {
+
+  private http = inject(HttpClient);
+
+  private baseUrl = 'http://localhost:8000/tickets';
+
+  listar(): Observable<TicketResposta[]> {
+    return this.http.get<TicketResposta[]>(this.baseUrl);
+  }
+}
